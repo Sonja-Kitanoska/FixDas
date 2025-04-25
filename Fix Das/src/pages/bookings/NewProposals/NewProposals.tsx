3e8ad67424e56f924c416eb35aec4330bfc6de1a
Sonja Kitanoska <avramoskamkd@gmail.com>
2025-04-25 23:29:14 +0200

    navigate to every step of the bookings

--- Fix Das/src/pages/bookings/NewProposals/NewProposals.tsx
+++ Fix Das/src/pages/bookings/NewProposals/NewProposals.tsx
@@ -1,7 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import Navbar from "../../../components/Navbar/Navbar";
 import styles from "../Bookings.module.css";
 
 const NewProposals = () => {
+	const navigate = useNavigate();
+
 	return (
 		<>
 			<div className="container py-3">
@@ -13,12 +16,23 @@ const NewProposals = () => {
 					className={`d-flex justify-content-between mt-3 py-2 ${styles.grayFont}`}
 				>
 					<p
+						onClick={() => navigate("/bookings/new-proposals")}
 						className={`font-size-14 font-weight-500 orange ${styles.orangeBorder}`}
 					>
 						New Proposals (1)
 					</p>
-					<p className="font-size-14 font-weight-500">Ongoing</p>
-					<p className="font-size-14 font-weight-500">Completed</p>
+					<p
+						onClick={() => navigate("/bookings/ongoing")}
+						className="font-size-14 font-weight-500"
+					>
+						Ongoing
+					</p>
+					<p
+						onClick={() => navigate("/bookings/completed")}
+						className="font-size-14 font-weight-500"
+					>
+						Completed
+					</p>
 				</div>
 
 				<div className="card">
--- Fix Das/src/pages/bookings/OngoingBooking/OngoingBooking.tsx
+++ Fix Das/src/pages/bookings/OngoingBooking/OngoingBooking.tsx
@@ -1,7 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import Navbar from "../../../components/Navbar/Navbar";
 import styles from "../Bookings.module.css";
 
 const OngoingBooking = () => {
+	const navigate = useNavigate();
+
 	return (
 		<div className="container py-3">
 			<div className="d-flex justify-content-between align-items-center py-2">
@@ -11,9 +14,24 @@ const OngoingBooking = () => {
 			<div
 				className={`d-flex justify-content-between mt-3 py-2 ${styles.grayFont}`}
 			>
-				<p className="font-size-14 font-weight-500">New Proposals (1)</p>
-				<p className={`font-size-14 font-weight-500 orange ${styles.orangeBorder}`}>Ongoing</p>
-				<p className="font-size-14 font-weight-500">Completed</p>
+				<p
+					onClick={() => navigate("/bookings/new-proposals")}
+					className="font-size-14 font-weight-500"
+				>
+					New Proposals (1)
+				</p>
+				<p
+					onClick={() => navigate("/bookings/ongoing")}
+					className={`font-size-14 font-weight-500 orange ${styles.orangeBorder}`}
+				>
+					Ongoing
+				</p>
+				<p
+					onClick={() => navigate("/bookings/completed")}
+					className="font-size-14 font-weight-500"
+				>
+					Completed
+				</p>
 			</div>
 
 			<Navbar />
