a342ac52c4f7f15a13ddca41ad0f531be417428a
Sonja Kitanoska <avramoskamkd@gmail.com>
2025-04-26 21:51:07 +0200

    bookings cards for all the steps done

--- Fix Das/src/pages/bookings/OngoingBooking/OngoingBooking.tsx
+++ Fix Das/src/pages/bookings/OngoingBooking/OngoingBooking.tsx
@@ -1,10 +1,10 @@
-import { useNavigate } from "react-router-dom";
 import Navbar from "../../../components/Navbar/Navbar";
 import styles from "../Bookings.module.css";
+import { LuClock3, LuMapPin } from "react-icons/lu";
+import { FaRegCalendarCheck } from "react-icons/fa6";
+import { PiArrowsClockwiseFill, PiUserGearLight } from "react-icons/pi";
 
 const OngoingBooking = () => {
-	const navigate = useNavigate();
-
 	return (
 		<div className="container py-3">
 			<div className="d-flex justify-content-between align-items-center py-2">
@@ -14,24 +14,63 @@ const OngoingBooking = () => {
 			<div
 				className={`d-flex justify-content-between mt-3 py-2 ${styles.grayFont}`}
 			>
+				<p className="font-size-14 font-weight-500">New Proposals (1)</p>
 				<p
-					onClick={() => navigate("/bookings/new-proposals")}
-					className="font-size-14 font-weight-500"
-				>
-					New Proposals (1)
-				</p>
-				<p
-					onClick={() => navigate("/bookings/ongoing")}
 					className={`font-size-14 font-weight-500 orange ${styles.orangeBorder}`}
 				>
 					Ongoing
 				</p>
-				<p
-					onClick={() => navigate("/bookings/completed")}
-					className="font-size-14 font-weight-500"
-				>
-					Completed
-				</p>
+				<p className="font-size-14 font-weight-500">Completed</p>
+			</div>
+
+			<div className="card">
+				<div className="card-body">
+					<div className="d-flex justify-content-between border-bottom py-1">
+						<h5 className="card-title font-size-14 font-weight-700">
+							Plumbing service
+						</h5>
+						<div className="d-flex orange align-items-center gap-1">
+							<LuMapPin />
+							<p className="mb-0 font-size-12 font-weight-600">View on map</p>
+						</div>
+					</div>
+					<div className="d-flex justify-content-between py-3 font-size-12 font-weight-400">
+						<div className="f-flex flex-column">
+							<div className="d-flex align-items-center gap-2 mb-2">
+								<FaRegCalendarCheck className="orange font-size-14" />
+								<p className="mb-0">Wed, 8 Nov</p>
+							</div>
+							<div className="d-flex align-items-center mb-2 gap-2">
+								<PiArrowsClockwiseFill
+									className="orange"
+									size={15}
+									strokeWidth={12}
+								/>
+								<p className="mb-0">Fix a leaking pipe</p>
+							</div>
+							<div className="d-flex align-items-center mb-2 gap-2">
+								<PiUserGearLight
+									className="orange"
+									size={15}
+									strokeWidth={12}
+								/>
+								<p className="mb-0">Klaus Schneider</p>
+							</div>
+						</div>
+						<div>
+							<div className="d-flex gap-2 align-items-center mb-1">
+								<LuClock3 className="orange" size={15} />
+								<p className="mb-0">Morning 9 am</p>
+							</div>
+						</div>
+					</div>
+
+					<div className="d-flex gap-2">
+						<button className="btn orange-border-btn">Chat now</button>
+
+						<button className="btn orange-btn">Completed</button>
+					</div>
+				</div>
 			</div>
 
 			<Navbar />
