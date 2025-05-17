a3affb6339607c65a1d9cbf2daa0c1b6ab75b499
Sonja Kitanoska <avramoskamkd@gmail.com>
2025-05-17 10:03:18 +0200

    view button on find-handyman page done

--- Fix Das/src/pages/FindHandyman/components/ClientFindHandyman/ClientFindHandyman.tsx
+++ Fix Das/src/pages/FindHandyman/components/ClientFindHandyman/ClientFindHandyman.tsx
@@ -7,10 +7,12 @@ import PostRequest from "../PostRequest/PostRequest";
 import { fetchHandymen } from "../../../../api/handymen";
 import { Handyman } from "../../../../types/types";
 import { useLocation } from "react-router-dom";
+import { BiSolidDownArrow } from "react-icons/bi";
 
 const ClientFindHandyman = () => {
 	const [visibleCount, setVisibleCount] = useState(3);
 	const [handymen, setHandymen] = useState<Handyman[]>([]);
+	const [isVisible, setIsVisible] = useState(true);
 
 	const location = useLocation();
 	const params = new URLSearchParams(location.search);
@@ -49,6 +51,10 @@ const ClientFindHandyman = () => {
 		setVisibleCount((prev) => prev + 3);
 	};
 
+	const toggleVisibility = () => {
+		setIsVisible((prev) => !prev);
+	};
+
 	const filteredHandymen = handymen.filter((handyman) => {
 		if (specialtiesFilter.length > 0) {
 			return handyman.specialties?.some((spec) =>
@@ -74,19 +80,34 @@ const ClientFindHandyman = () => {
 					redirectOnEnter={true}
 				/>
 			</div>
-			<div className="py-3 pt-3 container">
-				{filteredHandymen.length === 0 ? (
-					<div className="text-center">
-						<p>No handymen found matching your search criteria.</p>
+			<div className="container pt-4">
+				<div
+					onClick={toggleVisibility}
+					className="d-flex justify-content-between  py-1 border-bottom font-size-14"
+					style={{ color: "#939393", cursor: "pointer" }}
+				>
+					<p className="mb-0">{filteredHandymen.length} results</p>
+					<div className="d-flex align-items-center gap-2">
+						<p className="mb-0">View</p>
+						<BiSolidDownArrow size={10} />
 					</div>
-				) : (
-					filteredHandymen
-						.slice(0, visibleCount)
-						.map((handyman) => (
-							<HandymanCard key={handyman.id} handyman={handyman} />
-						))
-				)}
+				</div>
 			</div>
+			{isVisible && (
+				<div className="py-3 pt-3 container">
+					{filteredHandymen.length === 0 ? (
+						<div className="text-center">
+							<p>No handymen found matching your search criteria.</p>
+						</div>
+					) : (
+						filteredHandymen
+							.slice(0, visibleCount)
+							.map((handyman) => (
+								<HandymanCard key={handyman.id} handyman={handyman} />
+							))
+					)}
+				</div>
+			)}
 
 			{visibleCount < filteredHandymen.length && (
 				<div className="mt-3 d-flex justify-content-center">
